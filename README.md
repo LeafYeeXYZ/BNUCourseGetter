b0e213e89d7905ff1f6d62f0df9f2e0f6869ca5e
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-05-04 16:02:56 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+**开发中**
+
 # BNUCourseGetter
 基于 Wails / Go 和 React / TypeScript 的北师大自动抢课程序
---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import { GetTimetable } from '../wailsjs/go/main/App'
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+
+  const [status, setStatus] = useState<string>('空闲')
+
+  function timetable() {
+    const studentID = prompt("Enter your student ID")
+    const password = prompt("Enter your password")
+    const isBusy = confirm("Are system busy?")
+    if (!studentID || !password) {
+      alert("Please enter your student ID and password")
+      return
+    }
+
+    setStatus('获取中...')
+
+    GetTimetable(studentID, password, isBusy)
+      .then(() => {
+        setStatus('空闲')
+        alert('课程表截图已保存')
+      })
+      .catch((e: Error) => {
+        alert(`Error: ${e.message}`)
+      })
+  }
+
+  return (
+    <main id="container">
+      <button onClick={timetable}>获取课程表并保存截图</button>
+      <p>Status: {status}</p>
+    </main>
+  )
+}
+
+export default App
